19604d9beb8ad3599ab3578c487337a10cf12585
Pedro Luis Vergara <gudurix@MacBook-Pro-de-Pedro.local>
2020-03-23 18:42:04 +0100

    test heroku

--- src/services/IronClinicService.js
+++ src/services/IronClinicService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://ironclinic-api.herokuapp.com',
     withCredentials: true
 })
 
